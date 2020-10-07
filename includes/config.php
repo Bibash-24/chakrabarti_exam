37924575ec2497085b9a054405d9bf1fef63b1b6
arun rai <arunrai204@gmail.com>
2020-10-07 12:50:25 +0545

    config file change

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','srms');
+define('DB_NAME','chakrabarti');
 // Establish database connection.
 try
 {
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(isset($_POST['login']))
 {
 $uname=$_POST['username'];
 $password=md5($_POST['password']);
-$sql ="SELECT UserName,Password FROM admin WHERE UserName=:uname and Password=:password";
+$sql ="SELECT UserName,Password FROM user WHERE UserName=:uname and Password=:password";
 $query= $dbh -> prepare($sql);
 $query-> bindParam(':uname', $uname, PDO::PARAM_STR);
 $query-> bindParam(':password', $password, PDO::PARAM_STR);
